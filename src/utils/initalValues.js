7749a1a618e10ec0892338c06ed47e5fc48b41c3
yigitus <46648645+yigitus@users.noreply.github.com>
2024-10-31 12:21:00 +0300

    Add top panel with settings button and update styles

--- src/utils/initalValues.js
+++ src/utils/initalValues.js
@@ -1,6 +1,6 @@
 
 export const initialScript = `console.log("Hello World!");`;
-export const initialStyle = `body { background-color: #0E1218; }
+export const initialStyle = `body { background-color: #FFF; }
 h1, p { color: #FFF; }`;
 export const initialBody = `<p>Hello</p>`;
 export const initialPreviewSrc = `<!doctype html><html><head><style>${initialStyle}</style></head><body>${initialBody}<script>${initialScript}</script></body></html>`;
